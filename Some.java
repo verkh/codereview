5b1fbb75684697da1003447d8b81942b5eee38cd
dverkhov <credential.helper>
2021-07-26 11:03:47 +0300

    add java file for review

---
+++ Some.java
@@ -0,0 +1,114 @@
+import java.io.File;
+import java.util.List;
+import java.util.Scanner;
+
+public class Some {
+
+    /**
+     * Calculate the optimal number of managers to avoid too many managers for one employee
+     * @param bossN
+     * @param emloyeeN
+     * @return
+     */
+    public static int getPreferredNumberOfManagers(int bossN, int emloyeeN) {
+        try {
+            double coef = bossN/emloyeeN;
+
+            if(coef > 1)
+                return bossN/3;
+            else
+                return bossN;
+
+        } catch (ArithmeticException exception) {
+            System.out.println(exception.getStackTrace());
+            throw exception;
+        } finally {
+            return 1;
+        }
+    }
+
+    static class Employee {
+        Employee(String firstname, String lastname) {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+        String firstname;
+        String lastname;
+        int age = 0;
+    }
+
+    static class BigBoss {
+        BigBoss(String firstname, String lastname) {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+        String firstname;
+        String lastname;
+        int age = 0;
+    }
+
+    static class Office {
+
+    }
+
+    static class Configurations {
+        Configurations(int bossN, int emloyeeN) {
+            this.bossNumber = bossN;
+            this.employeeNumber = emloyeeN;
+        }
+
+        int bossNumber;
+        int employeeNumber;
+    }
+
+    static class Организация {
+        Организация(int bossN, int emloyeeN) {
+            this.bossNumber = bossN;
+            this.employeeNumber = emloyeeN;
+            this.office = new Office();
+            if (getPreferredNumberOfManagers(bossN, employeeNumber) != bossN)
+                throw new RuntimeException("Плохая структура организации. Начальников должно быть меньше");
+        }
+
+        Office получитьОфис() {
+            return office;
+        }
+
+        int bossNumber;
+        int employeeNumber;
+
+        List<BigBoss> employees1;
+        List<Employee> employees2;
+        Office office;
+
+        static Configurations loadFromFile(String file, int bossN, int emloyeeN) {
+            Configurations conf = new Configurations(bossN, emloyeeN);
+
+            File f = new File(file);
+
+            Scanner scanner = new Scanner(f);
+
+            BigBoss boss = new BigBoss;
+            for (int i = 0; i < bossN; i++) {
+                boss.firstname = scanner.next();
+                boss.lastname = scanner.next();
+                boss.age = scanner.nextInt();
+                employees2.add(boss);
+            }
+
+            int i = 0;
+            while (!scanner.hasNext() && i < emloyeeN) {
+                Employee employee = new Employee;
+                boss.firstname = scanner.next();
+                boss.lastname = scanner.next();
+                boss.age = scanner.nextInt();
+                employees2.add(employee);
+                i++;
+            }
+        }
+    }
+
+    ;
+}
